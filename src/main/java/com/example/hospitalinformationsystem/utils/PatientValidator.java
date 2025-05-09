7b9cf9ca00c61ae56358dcbb7dd943f44c1fbfef
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-09 02:48:17 -0700

    Added a Patient validator to check for empty fields

--- src/main/java/com/example/hospitalinformationsystem/utils/PatientValidator.java
+++ src/main/java/com/example/hospitalinformationsystem/utils/PatientValidator.java
@@ -1,4 +1,16 @@
 package com.example.hospitalinformationsystem.utils;
 
+import com.example.hospitalinformationsystem.models.Patient;
+
 public class PatientValidator {
+    public static boolean isValid(Patient patient) {
+        if (patient == null) return false;
+        if (patient.getPatientNumber() == null || patient.getPatientNumber().isBlank()) return false;
+        if (patient.getName() == null || patient.getName().isBlank()) return false;
+        if (patient.getSurname() == null || patient.getSurname().isBlank()) return false;
+        if (patient.getAddress() == null || patient.getAddress().isBlank()) return false;
+        if (patient.getTelephoneNumber() == null || !patient.getTelephoneNumber().matches("\\d{10,15}")) return false;
+
+        return true;
+    }
 }
